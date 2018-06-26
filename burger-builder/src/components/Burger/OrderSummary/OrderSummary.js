1dbc8b826d77f93902572d459e165a6d691a50af
Shreyansh Agarwal <agarwal.shreyansh131@gmail.com>
2018-06-26 19:20:18 +0530

    added price to the order summary modal and also added a toolbar

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/auxx';
+import Button from '../../UI/Button/Button';
 
 
 const orderSummary = (props) => {
@@ -14,7 +15,10 @@ const orderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
+      <p><strong>Total price: {props.price.toFixed(2)}</strong></p>
       <p>Continue to checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
     </Aux>
   );
 };
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -74,6 +74,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false});
   }
 
+  purchaseContinuedHandler = () => {
+    alert("You Continue");
+  }
+
   render(){
     const disabledInfo = {
       ...this.state.ingredients
@@ -84,7 +88,11 @@ class BurgerBuilder extends Component {
     return(
       <Aux>
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary ingredients={this.state.ingredients}/>
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinuedHandler}
+            price={this.state.totalPrice}/>
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
         <BuildControls
